c4e7bea212c47a1907ab45f54b3ce7a5047bf6a1
Syafiq Aiman <isyafiq@gmail.com>
2025-07-12 11:06:24 +0800

    feat: upload only images

--- app/events/components/UploadFileButton.tsx
+++ app/events/components/UploadFileButton.tsx
@@ -1,21 +1,25 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
+  overflow: "hidden",
+  position: "absolute",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
+  whiteSpace: "nowrap",
   width: 1,
 });
 
-export default function InputFileUpload({onChange}: {onChange: (fileList: FileList | null) => void}) {
+export default function InputFileUpload({
+  onChange,
+}: {
+  onChange: (fileList: FileList | null) => void;
+}) {
   return (
     <Button
       component="label"
@@ -29,6 +33,7 @@ export default function InputFileUpload({onChange}: {onChange: (fileList: FileLi
         type="file"
         onChange={(event) => onChange(event.target.files)}
         multiple
+        accept="image/*"
       />
     </Button>
   );
