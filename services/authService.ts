c858ceaabb020a6f6005fc634a5e65c111f3ce9d
Syafiq Aiman <isyafiq@gmail.com>
2025-07-12 14:56:25 +0800

    chore: get profile service

--- services/authService.ts
+++ services/authService.ts
@@ -8,7 +8,7 @@ export const login = async (email: string, password: string) => {
     headers: { "content-type": "application/json" },
     body: JSON.stringify({ email, password }),
   });
-  
+
   if (!res.ok) {
     const error = await res.json();
     throw new Error(error.message || "Failed to login");
@@ -17,7 +17,12 @@ export const login = async (email: string, password: string) => {
   return await res.json();
 };
 
-export const register = async (email: string, password: string, name: string, role: Role) => {
+export const register = async (
+  email: string,
+  password: string,
+  name: string,
+  role: Role,
+) => {
   const url = `http://localhost:9000/auth/register`;
 
   const res = await fetch(url, {
@@ -25,10 +30,30 @@ export const register = async (email: string, password: string, name: string, ro
     headers: { "content-type": "application/json" },
     body: JSON.stringify({ email, password, name, role }),
   });
-  
+
   if (!res.ok) {
     const error = await res.json();
     throw new Error(error.message || "Failed to register");
+  }
+
+  return await res.json();
+};
+
+export const getProfile = async () => {
+  const token = localStorage.getItem("accessToken");
+  if (!token) throw new Error("No token found");
+
+  const url = `http://localhost:9000/auth/profile`;
+
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message || "Failed to get profile");
   }
 
   return await res.json();
