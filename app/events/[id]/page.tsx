9140ddc1f1a0c27775683f8aaa5209176e9edc40
Syafiq Aiman <isyafiq@gmail.com>
2025-07-13 14:56:46 +0800

    feat: event details stylse

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,19 +1,30 @@
-"use client"
+"use client";
 import { useGetOneEvent } from "@/app/admin/events/hooks/useGetOneEvent";
+import { Box, Container, Grid, Typography } from "@mui/material";
 
 export default function showEventPage() {
-    const {data: event} = useGetOneEvent();
+  const { data: event } = useGetOneEvent();
 
-    if(!event) {
-        return <div></div>
-    }
-    
-    return <div>
+  if (!event) {
+    return <Box>Event not found</Box>;
+  }
+
+  return (
+    <Box sx={{ p: 2 }} display={'flex'} flexDirection={'column'} alignItems={'center'}>
+      <img
+        src={`http://localhost:9000${event.posterUrl}`}
+        alt={event.name}
+        style={{ maxHeight: "800px", borderRadius: "8px" }}
+      />
+      <Typography variant="h5" gutterBottom>
         {event.name}
-        {event.status}
-        {event.startDate}
-        {event.location}
-        {event.endDate}
-        <img src={`http://localhost:9000${event.posterUrl}`} alt={event.name} />
-    </div>
+      </Typography>
+      <Typography variant="body1">
+        {new Date(event.startDate).toDateString()} -{" "}
+        {new Date(event.endDate).toDateString()}
+      </Typography>
+      <Typography variant="body1">Location: {event.location}</Typography>
+      <Typography variant="body1">Status: {event.status}</Typography>
+    </Box>
+  );
 }
